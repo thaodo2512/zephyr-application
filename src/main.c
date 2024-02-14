ccdbe079f89bef644d79ee634bc6bfebd17ab7ac
Tinh Nguyen <tinh.nguyentrung1999@gmail.com>
2024-02-14 13:22:13 +0700

    init repo
    Signed-off-by: Tinh Nguyen <tinh.nguyentrung1999@gmail.com>

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/**
+ * @file main.c
+ * @brief Main file for the project
+ * @details This file contains the main function for the project
+ * @date 2024-02-14
+ * @author Tinh Nguyen
+ * @version 1.0
+ */
+
+#include <zephyr/kernel.h>
+
+int main(void)
+{
+    printk("Hello World\n");
+    return 0;
+}
